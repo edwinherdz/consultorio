c1ab5506fb035fcd178ac7415457bbc65ccd7591
edwinherdz <edwin.hdz93@gmail.com>
2023-06-21 15:06:36 -0600

    se cmenta botones del home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,9 @@
 @section('content')
 <h1>Pagina Principal</h1>
 @auth
-<button onclick="window.location.href='{{route('consultas.create')}}'">Crear consulta</button>
-{{ auth()->user()->name }}
+{{-- <button onclick="window.location.href='{{route('consultas.create')}}'">Crear consulta</button>
+{{ auth()->user()->name }} --}}
+En este pagina se definiran metricas , graficas etc. el cual en esta primera fase no esta contemplado
 
 @endauth
 
