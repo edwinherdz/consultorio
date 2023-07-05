c32f74d3fdd529fec96f11461bf3fa2c26279efe
edwinherdz <edwin.hdz93@gmail.com>
2023-07-05 15:43:42 -0600

    se elimina la clase de fondo para identificar el usuario

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -21,7 +21,7 @@
              </div>
              <div class="">
                  <div class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                     <div id="menu-button" aria-expanded="true" aria-haspopup="true" class="bg-gray-700">
+                     <div id="menu-button" aria-expanded="true" aria-haspopup="true" >
                          <a href="{{route('usuario.show',auth()->user()->id)}}">Hola {{ auth()->user()->name }}</a>
                      </div>
                      <div id="menu-button" aria-expanded="true" aria-haspopup="true">
