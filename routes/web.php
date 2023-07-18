6fd2201435cff8129a1df944c43a7501e98aef83
edwinherdz <edwin.hdz93@gmail.com>
2023-07-18 12:21:17 -0600

    optimización de rutas

--- routes/web.php
+++ routes/web.php
@@ -12,38 +12,15 @@ use App\Http\Controllers\UserController;
 use App\Models\Estudio;
 
 Route::view('/','home')->name('home')->middleware('auth');
-
 Auth::routes();
-
 Route::get('/datosconsultorio',[ ConsultorioController::class,'index'])->name('consultorio.index')->middleware('auth');
 Route::resource('consulta',ConsultaController::class)->parameters(['consulta'=>'consulta'])->names('consultas')->middleware('auth');
 Route::get('/consulta/pdf/consultaslist',[PdfController::class,'pdflist'])->name('consultas.pdflist')->middleware('auth');
 Route::resource('paciente',PacienteController::class)->names('pacientes')->middleware('auth');
 Route::get('/consulta/pdf/{consulta}',[PdfController::class,'pdf'])->name('consultas.pdf')->middleware('auth');
 Route::resource('consultorio', ConsultorioController::class)->names('consultorios')->middleware('auth');
-
 Route::resource('usuarios',UserController::class)->names('usuarios')->middleware('auth');
-
 Route::resource('consultorio', ConsultorioController::class)->names('consultorios')->middleware('auth');
 Route::resource('estudio', EstudioController::class)->names('estudios')->middleware('auth');
-
-// Route::get('/consulta',[ConsultaController::class,'index'])->name('consultas.index')->middleware('auth');
-// Route::get('/paciente',[PacienteController::class,'index'])->name('pacientes.index')->middleware('auth');
-
-// Route::get('/consulta/crear',[ConsultaController::class,'create'])->name('consultas.create')->middleware('auth');
-// Route::post('/consulta',[ConsultaController::class,'store'])->name('consultas.store')->middleware('auth');
-
-
-// Route::get('/{consulta}/editar',[ConsultaController::class,'edit'])->name('consultas.edit')->middleware('auth');
-// Route::patch('/consulta/{consulta}', [ConsultaController::class,'update'])->name('consultas.update')->middleware('auth');
-// Route::get('/{consulta}',[ConsultaController::class,'show'])->name('consultas.show')->middleware('auth');
-
-
-// Route::post('/paciente',[PacienteController::class,'store'])->name('pacientes.store')->middleware('auth');
-// Route::get('/paciente/crear',[PacienteController::class,'create'])->name('pacientes.create')->middleware('auth');
-// Route::patch('/paciente/{consulta}', [PacienteController::class,'update'])->name('pacientes.update')->middleware('auth');
-// Route::get('/paciente/{paciente}/editar',[PacienteController::class,'edit'])->name('pacientes.edit')->middleware('auth');;
-// Route::get('/paciente/{paciente}',[PacienteController::class,'show'])->name('pacientes.show')->middleware('auth');
-
 
 
