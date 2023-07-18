b12a4c4604d067be751b92e68941b0141ee043fd
edwinherdz <edwin.hdz93@gmail.com>
2023-07-18 15:39:36 -0600

    se agrega icono

--- resources/views/partials/side_bar.blade.php
+++ resources/views/partials/side_bar.blade.php
@@ -3,10 +3,11 @@
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'bg-cyan-300':'' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white-100 dark:hover:bg-teal-500  hover:bg-cyan-400">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentcolor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+                      </svg>
+                      
                     <span class="ml-3">Dashboard</span>
                 </a>
             </li>
